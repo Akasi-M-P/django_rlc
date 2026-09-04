081c58ab1e84c4ba478d35ac88a08a9df1bc3b29
Peter Akasi Mba <awintechworld@gmail.com>
2026-09-04 21:29:05 +0000

    urls and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!",
+}
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+    
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenges_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("The month is invalid!")
+    
+    redirect_month = months[month - 1]
+    # reverse() function is used to get the URL of a view by its name. It takes the name of the view and any arguments that the view requires, and returns the URL as a string.
+    redirect_url = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_url)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except KeyError:
+        return HttpResponseNotFound("This month is not supported!")
+
