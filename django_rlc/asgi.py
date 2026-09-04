d5fc1b8fe0180bbaade3b1b174726617fce9528e
Peter Akasi Mba <awintechworld@gmail.com>
2026-09-04 02:36:38 +0000

    Initial commit: Django project setup with challenges app

---
+++ django_rlc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for django_rlc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application  # pyright: ignore[reportMissingImports,reportMissingModuleSource]
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rlc.settings')
+
+application = get_asgi_application()
